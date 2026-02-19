817c030c05a22d769ec7aad2c6d90e94d682884b
DBrownDHen <dbrownki@udel.edu>
2026-02-19 16:30:20 -0500

    Adds functionality to Status Effect Prototype
    Technically, the prototype works. It can apply and execute effects simultaneously; however, I ran into some issues.
    Health: To get health to work properly, it needed to be treated like a float, which means that a player could have 0.5 health in some cases. Obviously, we probably don't want that, so that will need to be fixed in some way.
    Effects Storage: To manage the status effects, I stored them into two different Arrays: one for the effect name, and another for the effect duration. My goal in mind is to remove the effect when its corresponding duration reaches 0, but (to nobody's surprise) I have run into errors when attempting to remove values in the arrays. As a temporary solution, when the effect's time is up, it is set to null, so it doesn't do anything else. However, this could create performance issues later on if the player accumulates more status effects, and the arrays (filled with empty values) become too large to handle.

--- too-many-cooks/Scripts/prototype stuff/Effects Prototype/se_prototype.gd
+++ too-many-cooks/Scripts/prototype stuff/Effects Prototype/se_prototype.gd
@@ -1,6 +1,9 @@
 extends Node2D
 @export var health = 100
+@export var displayHealth: int
 @export var maxHealth = 100
+var statusEffects = []
+var effectDurations = []
 
 signal applyPoison(duration)
 signal applyRegeneration(duration)
@@ -14,5 +17,45 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$HealthBar/HealthValue.text = str(health)
+	for i in range(statusEffects.size()):
+		effectDurations[i] -= 1.0 * delta 
+		if (effectDurations[i] <= 0):
+			effectDurations[i] = -1
+			statusEffects[i] = null
+
+	if (statusEffects.has("Poison")):
+		health -= 2.0 * delta
+	if (statusEffects.has("Regeneration")):
+		health += 1.0 * delta
+	displayHealth = int(health)
+	
+	if (health > maxHealth):
+		health = maxHealth
+	
+	
+	print(statusEffects)
+	print(effectDurations)
+	$HealthBar/HealthValue.text = str(displayHealth)
 	$HealthBar.value = health
+
+func _on_poison_button_pressed() -> void:
+	applyPoison.emit(5)
+
+
+
+func _on_heal_button_pressed() -> void:
+	applyRegeneration.emit(5)
+
+
+func _on_effect_clear_button_pressed() -> void:
+	statusEffects = []
+	effectDurations = []
+
+
+func _on_apply_poison(duration: Variant) -> void:
+	statusEffects.append("Poison")
+	effectDurations.append(duration)
+
+func _on_apply_regeneration(duration: Variant) -> void:
+	statusEffects.append("Regeneration")
+	effectDurations.append(duration)
