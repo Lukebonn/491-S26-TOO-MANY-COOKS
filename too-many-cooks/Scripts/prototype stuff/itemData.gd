baad40b29a9c7397268583e96e4c8b2e4852baba
Owen Magee <owenmagee7@gmail.com>
2026-02-19 18:27:37 -0500

    Added function definitions to inventory methods
    - _ready
    - add_item
    - remove_item
    - remove_item_from_slot
    - get_item_from
    - get_slot_with
    - get_empty_slot
    - check_inventory
    
    Also added @export to itemData variables

--- too-many-cooks/Scripts/prototype stuff/itemData.gd
+++ too-many-cooks/Scripts/prototype stuff/itemData.gd
@@ -1,7 +1,7 @@
 class_name ItemData
 extends Resource
 
-var name : String
-var decription : String
-var max_stack : int
-var image : Texture
+@export var name : String
+@export var decription : String
+@export var max_stack : int
+@export var icon : Texture
--- too-many-cooks/Scripts/prototype stuff/ui stuff/inventory.gd
+++ too-many-cooks/Scripts/prototype stuff/ui stuff/inventory.gd
@@ -1,5 +1,5 @@
 class_name Inventory
-extends Resource
+extends Node
 
 class ItemSlot:
 	var item : ItemData
@@ -13,16 +13,22 @@ var item_slots : Array[ItemSlot]
 signal inventory_updated
 signal slot_updated
 
-#instantiates new ItemSlots equal to inv_size
+##instantiates new ItemSlots equal to inv_size
+#adds items from starting_items to item_slots
 func _ready() -> void:
 	for i in range(inv_size):
 		item_slots.append(ItemSlot.new())
+	
+	for key in starting_items:
+		for i in range(starting_items[key]):
+			add_item(key)
 
-##adds item to available emptyslot
+##adds item to slot containing that item or to an empty slot
+#item: ItemData to be added
 #returns false if item cannot be added
 func add_item(item : ItemData) -> bool:
-	var slot = get_slot(item)
-	if slot.quantity >= item.max_stack:
+	var slot = get_slot_with(item)
+	if slot == null or slot.quantity >= item.max_stack:
 		slot = get_empty_slot()
 		
 		if(slot == null):
@@ -33,22 +39,61 @@ func add_item(item : ItemData) -> bool:
 	else:
 		slot.quantity += 1
 	
+	inventory_updated.emit()
+	slot_updated.emit()
 	return true
 
+##removes item from inventory
+#item: ItemData to be removed
 func remove_item(item : ItemData):
-	pass
+	var slot = get_slot_with(item)
+	slot.item = null
+	slot.quantity = 0
+	
+	inventory_updated.emit()
+	slot_updated.emit()
 
-func get_item(slot : ItemSlot) -> ItemData:
+##removes item from specified slot
+#slot: ItemSlot to be emptied
+func remove_item_from_slot(slot : ItemSlot):
+	slot.item = null
+	slot.quantity = 0
+	
+	inventory_updated.emit()
+	slot_updated.emit()
+
+##returns the item stored in slot parameter
+#slot: ItemSlot to be searched
+func get_item_from(slot : ItemSlot) -> ItemData:
+	return slot.item
+
+##returns the item slot that has the item in the parameter
+#item: ItemData that specifies the item to be found
+func get_slot_with(item : ItemData) -> ItemSlot:
+	for slot in item_slots:
+		if slot.item == item:
+			return slot
+	
 	return null
 
-func get_slot(item : ItemData) -> ItemSlot:
-	return null
-
+##returns the first empty slot
+#returns null if no empty slots are found
 func get_empty_slot() -> ItemSlot:
+	for slot in item_slots:
+		if slot.item == null:
+			return slot
+	
 	return null
 
+##checks inventory for item in parameter
+#item: ItemData that the function is looking for
+#returns false if item is not found
 func check_inventory(item : ItemData) -> bool:
-	return true
+	for slot in item_slots:
+		if slot.item == item:
+			return true
+	
+	return false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
