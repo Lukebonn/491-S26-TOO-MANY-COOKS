6ab9087eb64c01f799fe4169735ced2f114aa943
Luke Bonniwell <113559516+Lukebonn@users.noreply.github.com>
2026-02-23 23:35:16 -0500

    Slime-enemy-addition
    I created the slime enemy, updated the player within the enemy test scene, added some animations for the slime and player, and added all the code for the slime and player.

--- too-many-cooks/Scripts/prototype stuff/Enemy Prototype/player.gd
+++ too-many-cooks/Scripts/prototype stuff/Enemy Prototype/player.gd
@@ -1,28 +1,57 @@
 extends CharacterBody2D
 
+const SPEED = 300
 
-const speed = 200
 var current_dir = "none"
+var last_direction: Vector2 = Vector2.RIGHT
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		current_dir="right"
-		velocity.x=speed
-		velocity.y=0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir="left"
-		velocity.x=-speed
-		velocity.y=0
-	elif Input.is_action_pressed("ui_up"):
-		current_dir="up"
-		velocity.x=0
-		velocity.y=-speed
-	elif Input.is_action_pressed("ui_down"):
-		current_dir="down"
-		velocity.x=0
-		velocity.y=speed	
-	else:
-		velocity.x=0
-		velocity.y=0
+func _physics_process(_delta: float) -> void:
+		
 	move_and_slide()
+	process_animation()
+	process_movement()
+	#if Input.is_action_pressed("right"):
+		#current_dir="right"
+		#velocity.x=speed
+		#velocity.y=0
+	#elif Input.is_action_pressed("left"):
+		#current_dir="left"
+		#velocity.x=-speed
+		#velocity.y=0
+	#elif Input.is_action_pressed("up"):
+		#current_dir="up"
+		#velocity.x=0
+		#velocity.y=-speed
+	#elif Input.is_action_pressed("down"):
+		#current_dir="down"
+		#velocity.x=0
+		#velocity.y=speed	
+	#else:
+		#velocity.x=0
+		#velocity.y=0
+		
+func process_movement() -> void:
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else: 
+		velocity = Vector2.ZERO
+
+func process_animation() -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("walk", last_direction)
+	else:
+		play_animation("idle", last_direction)
+
+func play_animation(prefix: String, dir: Vector2) -> void: 
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix + "_side")
+	if dir.y < 0:
+		animated_sprite_2d.play(prefix + "_back")
+	if dir.y > 0:
+		animated_sprite_2d.play(prefix + "_front")
---
+++ too-many-cooks/Scripts/prototype stuff/Enemy Prototype/slime_enemy.gd
@@ -0,0 +1,61 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const KNOCKBACK_FORCE: int = 100
+
+var is_alive: bool = true
+var health: int = 100
+var target = null
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var take_damage_sound: AudioStreamPlayer2D = $TakeDamage
+
+
+func _physics_process(delta: float) -> void:
+	if is_alive and target:
+		_attack(delta) 
+	move_and_slide()
+
+func _attack(delta: float) -> void:
+	var direction = (target.position - position).normalized()
+	position += direction * SPEED * delta
+	animated_sprite_2d.play("attack_side")
+	
+#function that can be called from the player to deal damage to the slime
+func take_damage(damage: int, attacker_position: Vector2) -> void: 
+	health -= damage
+	if health <= 0:
+		_die()
+	else:
+		print(health)
+		take_damage_sound.play() #pending audio to the take damage
+		
+		#Knockback
+		var knockback_direction = (position - attacker_position).normalized()
+		var target_position = position + knockback_direction * KNOCKBACK_FORCE
+		
+		var tween = create_tween()
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(self, "position", target_position, 0.5)
+	
+	
+func _die() -> void:
+	is_alive = false
+	animated_sprite_2d.play("death")
+	
+	#disable collision
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sight/CollisionShape2D.set_deferred("disabled", true)
+
+func _on_sight_body_entered(body: Node2D) -> void:
+	#print(body.name)
+	if body.name == "SwordEnemy":
+		target = body
+
+
+func _on_sight_body_exited(body: Node2D) -> void:
+	if body.name == "SwordEnemy" and is_alive:
+		target = null
+		animated_sprite_2d.play("idle_front")
