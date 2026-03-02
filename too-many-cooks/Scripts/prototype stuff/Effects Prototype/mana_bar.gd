78ddef922023b2714d0166570a50b44ec0713fc8
DBrownDHen <dbrownki@udel.edu>
2026-03-01 19:02:00 -0500

    Mana now regenerates over time.

--- too-many-cooks/Scripts/prototype stuff/Effects Prototype/mana_bar.gd
+++ too-many-cooks/Scripts/prototype stuff/Effects Prototype/mana_bar.gd
@@ -1,5 +1,5 @@
 extends ProgressBar
-
+var regenCooldown = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,8 +13,23 @@ func _process(delta: float) -> void:
 	$ManaValue.text = str(get_parent().displayMana)
 	
 	if Input.is_action_just_pressed("use_item"):
-		if (get_parent().mana >= 20):
+		if (get_parent().displayMana >= 20):
+			regenCooldown = true
+			$ManaTimer.start(3)
 			get_parent().mana -= 20
 			get_parent().applyStatusEffect.emit("Regeneration", 5)
 		else:
 			print("You don't have enough Mana to do this.")
+
+	if regenCooldown == false:
+		get_parent().mana += 2.0 * delta
+	
+	if get_parent().mana > get_parent().maxMana:
+		get_parent().mana = get_parent().maxMana
+	if get_parent().mana < 0:
+		get_parent().mana = 0
+	
+
+func _on_mana_timer_timeout() -> void:
+	regenCooldown = false
+	$ManaTimer.stop()
