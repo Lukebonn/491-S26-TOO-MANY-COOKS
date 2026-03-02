140bf9b68b7424f42f7f552702f70d837ecbbc86
mcheng11 <mcheng0439@gmail.com>
2026-03-02 02:35:39 -0500

    tweaked mini map. added a test map wit temp tile sets/mini map.
    tweaked mini map. added a test map wit temp tile sets.

--- too-many-cooks/Scripts/mini_map_sub_viewport.gd
+++ too-many-cooks/Scripts/mini_map_sub_viewport.gd
@@ -1,19 +1,18 @@
 #Everything is commented out for now!
 
-#extends SubViewport
+extends SubViewport
 
-#@export var player: CharacterBody2D #connect to player var
-#@onready var player: CharacterBody2D = $"../../../player" #not sure if we have a player alredy
+@onready var player: CharacterBody2D = $"../../../Player" #not sure if we have a player alredy
 
-#@onready var canera_2d: = $Camera2D
+@onready var camera_2d: = $Camera2D
 
 #fill in the sprite with what the player should be on the mini map
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#world_2d = get_tree().root.world_2d
+func _ready() -> void:
+	world_2d = get_tree().root.world_2d
 
 
 #Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _physics_process(delta:float) -> void:
+func _physics_process(delta:float) -> void:
 	#sets camera pos to player pos
-	#$Camera2D.position = player.position
+	$Camera2D.position = player.position
