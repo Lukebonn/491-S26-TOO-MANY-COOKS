b1897c0fe4f9cdedf07facea4ba33a615cc0c33f
YakuzaBalooza <jkrow124@gmail.com>
2026-03-06 11:58:03 -0500

    SaveMgr.Load() and Reset()
    - Added Load() to SaveMgr, can be used to restart the player at the last "checkpoint" (when the last Save() call occured, like at the beginning of a tower run).
    - Added Reset() to SaveMgr, deletes player save and creates a new one. Should only be used from the title screen (and preferably restart that scene too).

--- too-many-cooks/System/Save/SaveMgr.gd
+++ too-many-cooks/System/Save/SaveMgr.gd
@@ -2,11 +2,25 @@ extends Node
 const SavePath = "res://System/Save/SaveGame.tres"
 var GameSave: SaveGame = null
 
+# Attempt to load SaveGame via Load().
 func _ready() -> void:
-	if ResourceLoader.exists(SavePath):
-		GameSave = ResourceLoader.load(SavePath, "", ResourceLoader.CACHE_MODE_IGNORE)
-	else:
-		GameSave = SaveGame.new()
+	Load()
 
+# Save game data, creating a new SaveGame if one doesn't exist.
 func Save() -> void:
+	if ResourceLoader.exists(SavePath):
+		GameSave = SaveGame.new()
 	ResourceSaver.save(GameSave, SavePath)
+
+# Load game save, calling Save() if one doesn't exist.
+func Load() -> void:
+	if not ResourceLoader.exists(SavePath):
+		Save()
+	else:
+		GameSave = ResourceLoader.load(SavePath, "", ResourceLoader.CACHE_MODE_IGNORE)
+
+# Delete save data and create new data with Save().
+func Reset() -> void:
+	if ResourceLoader.exists(SavePath):
+		DirAccess.remove_absolute(SavePath)
+	Save()
