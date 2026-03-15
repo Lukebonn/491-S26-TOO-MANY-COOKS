1ee3761ab2ea478c6e2ab9afffa7e4d9d94a05db
tvn-ops <tvn@udel.edu>
2026-03-15 17:02:42 -0400

    Added a new font and finished up the tutorial prompt. Put tutorial prompt in Combat Test scene and made it disappear when the "OK" button was clicked.

--- too-many-cooks/tutorial_prompt.gd
+++ too-many-cooks/tutorial_prompt.gd
@@ -1,17 +1,19 @@
-extends ColorRect
+extends Node2D
 
-
+var first_time = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if first_time == false:
+		queue_free()
+	elif first_time == true:
+		show()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
+func _on_button_pressed():
 	queue_free()
-	$Label.visible = false
-	$Button.visible = false
+	first_time = false
