e99c9af2299f843def0ebce67088aabcb7dd0cd1
DBrownDHen <dbrownki@udel.edu>
2026-03-19 19:20:17 -0400

    Adds volume variables to "GameSave"
    The three different volume sliders in the settings now have a function in the code. Now, they each change a value that stores the player's volume settings. However, as of now, I don't believe that these saved values actually affect the volume of their respective "things."
    No other settings are being stored at the moment (since we don't have that many settings right now).

--- too-many-cooks/System/Save/SaveGame.gd
+++ too-many-cooks/System/Save/SaveGame.gd
@@ -7,3 +7,9 @@ extends Resource
 # The current level of the Tavern.
 # Starts at 0 for Level 1, then incriments (1=Lv2, etc.).
 @export var tavern_level := 0
+
+# These three variables store the selected volume for each 
+# type.
+@export var master_volume := 100.0
+@export var music_volume := 100.0
+@export var sound_volume := 100.0
