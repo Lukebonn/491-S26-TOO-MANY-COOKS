b7f77d00625ce2068a36fc36b677aac395ba9620
CSD-business <143720980+CSD-business@users.noreply.github.com>
2026-03-19 14:07:14 -0400

    Basic level editor. No doors/keys/chests yet

---
+++ too-many-cooks/Objects/PlayerCamera/player_camera.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+@export var Player : CharacterBody2D
+
+#for if we want to move the camera away from the character sometimes 
+#like to introduce a boss or something
+var is_following_player = true
+var sensitivity = 60
+func _ready():
+	#if there is no player set try to make it a sibling
+	if Player == null:
+		Player = $"../Player"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#TODO: make it so the camera is disjoint off the player slightly
+	#based on the vector of the 
+	
+	
+	if is_following_player:
+		position = Player.position + get_mouse_vector()/sensitivity
+
+func get_mouse_vector():
+	var value = Player.position + get_local_mouse_position()
+	print(value)
+	return value
