d175d23139a449a2a9f3f933e7ea80d9e492698f
DBrownDHen <dbrownki@udel.edu>
2026-04-05 13:06:39 -0400

    Added Hover/Click Sounds to Main Menu Buttons
    This commit also comes with a new Audio folder, which contains two separate Music and Sounds folders, so we can organize our game audio a little better

---
+++ too-many-cooks/Audio/Sounds/buttonSounds.gd
@@ -0,0 +1,12 @@
+extends Button
+
+@export var hoverSound : AudioStreamPlayer
+@export var clickSound : AudioStreamPlayer
+
+func _on_mouse_entered() -> void:
+	if hoverSound:
+		hoverSound.play()
+
+func _on_button_down() -> void:
+	if clickSound:
+		clickSound.play()
