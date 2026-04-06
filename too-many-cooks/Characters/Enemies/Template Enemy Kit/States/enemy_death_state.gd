b466c4004c9f80a78219132f11aaf42c0bcc6744
Shaina <shainaz@udel.edu>
2026-04-06 19:34:21 -0400

    Added enemy drop
    Enemy death now adds to the player's gold count in enemy template, this can be added upon later if we want to do other drops

--- too-many-cooks/Characters/Enemies/Template Enemy Kit/States/enemy_death_state.gd
+++ too-many-cooks/Characters/Enemies/Template Enemy Kit/States/enemy_death_state.gd
@@ -1,7 +1,12 @@
 extends EnemyState
 #this state is called when the enemy current_health = 0 VIA the hitstate
+@export var drop:int
+
 func enter_state(enemy_node):
 	super(enemy_node)
+	PlayerStats.Gold+=drop
 	if animation_name != "NONE":
 		await $"../AnimatedSprite2D".animation_finished
 	get_parent().queue_free()
+	
+	
