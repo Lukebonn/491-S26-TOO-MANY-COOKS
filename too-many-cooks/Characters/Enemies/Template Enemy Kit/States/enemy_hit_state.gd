7af55da2d7e585351847bda379c8048a2e39de4a
CSD-business <143720980+CSD-business@users.noreply.github.com>
2026-04-06 19:33:03 -0400

    Enemy knock back fix

--- too-many-cooks/Characters/Enemies/Template Enemy Kit/States/enemy_hit_state.gd
+++ too-many-cooks/Characters/Enemies/Template Enemy Kit/States/enemy_hit_state.gd
@@ -6,13 +6,18 @@ extends EnemyState
 
 ##how long enemy is "stunned" after hit, set 0 for none
 @export var Hit_Stun : float
+
+var knockback = Vector2.ZERO
+
 func enter_state(enemy_node):
 	super(enemy_node)
 	if enemy_ref.current_health <= 0:
 		exit_state()
 	if player_ref and enemy_ref:
 		var direction = (enemy_ref.position -player_ref.position).normalized()
-		enemy_ref.velocity = Knockback_Strength * direction
+		knockback = Knockback_Strength * direction
+		var tween = get_tree().create_tween()
+		tween.tween_property(self,"knockback",Vector2(0,0),Hit_Stun)
 		if Hit_Stun > 0 and enemy_ref.health >= 0:
 			await get_tree().create_timer(Hit_Stun).timeout
 			#$Timer.start(Hit_Stun)
@@ -20,8 +25,7 @@ func enter_state(enemy_node):
 		exit_state()
 
 func process(delta):
-	var tween = get_tree().create_tween()
-	tween.tween_property(enemy_ref,"velocity",Vector2(0,0),Hit_Stun)
+	enemy_ref.velocity = knockback
 	enemy_ref.move_and_slide()
 	
 func exit_state():
