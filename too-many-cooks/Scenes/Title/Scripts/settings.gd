34830fce99f585ba4178a260822142c1751beed7
CSD-business <143720980+CSD-business@users.noreply.github.com>
2026-04-10 12:12:30 -0400

    minor settings scripting, adds tileset props to Level Design/Props

--- too-many-cooks/Scenes/Title/Scripts/settings.gd
+++ too-many-cooks/Scenes/Title/Scripts/settings.gd
@@ -3,19 +3,21 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	show()
+	do_settings_action("show_menu")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		var tween = get_tree().create_tween()
-		tween.tween_property($cover,"position",Vector2(-1152,0),0.1).set_trans(Tween.TRANS_EXPO)
-		tween.tween_property($background,"position",$Marker2D.position,0.2).set_trans(Tween.TRANS_EXPO)
-		for sprite in $buttons.get_children():
-			tween.tween_property(sprite, "position", Vector2($Marker2D.position.x-160,sprite.position.y), 0.05).set_trans(Tween.TRANS_EXPO)
-	if Input.is_action_pressed("ui_right"):
-		var tween = get_tree().create_tween()
-		for sprite in $buttons.get_children():
-			tween.tween_property(sprite, "position", Vector2($Marker2D.position.x+160,sprite.position.y), 0.02).set_trans(Tween.TRANS_EXPO)
-		tween.tween_property($background,"position",$Marker2D2.position,0.1).set_trans(Tween.TRANS_EXPO)
-		tween.tween_property($cover,"position",Vector2(0,0),0.1).set_trans(Tween.TRANS_EXPO)
+func do_settings_action(action: String):
+	match action:
+		"show_menu":
+			var tween = get_tree().create_tween()
+			print(tween)
+			tween.tween_property($cover,"position",Vector2(-1152,0),0.1).set_trans(Tween.TRANS_EXPO)
+			tween.tween_property($background,"position",$Marker2D.position,0.2).set_trans(Tween.TRANS_EXPO)
+			for sprite in $buttons.get_children():
+				tween.tween_property(sprite, "position", Vector2($Marker2D.position.x-160,sprite.position.y), 0.05).set_trans(Tween.TRANS_EXPO)
+		"hide_menu":
+			var tween = get_tree().create_tween()
+			for sprite in $buttons.get_children():
+				tween.tween_property(sprite, "position", Vector2($Marker2D.position.x+160,sprite.position.y), 0.02).set_trans(Tween.TRANS_EXPO)
+			tween.tween_property($background,"position",$Marker2D2.position,0.1).set_trans(Tween.TRANS_EXPO)
+			tween.tween_property($cover,"position",Vector2(0,0),0.1).set_trans(Tween.TRANS_EXPO)
+		
