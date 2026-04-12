c66f649a14f59149b52d2b3696dc26678b7a08c6
YakuzaBalooza <jkrow124@gmail.com>
2026-04-12 19:40:40 -0400

    Minor UI Edits
    - Imported and added the Gold, Arrows, and Tower icons to relevant UI.
    - Added a UI display for how many Orbs the player has.
    - Decreased the size of the HP and MP bars in the UI scene to 0.65 scale. Also made minor adjustments to the placement of these elements.

--- too-many-cooks/UI/BattleHUD/orb_count.gd
+++ too-many-cooks/UI/BattleHUD/orb_count.gd
@@ -4,5 +4,5 @@ extends RichTextLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	text = str(PlayerStats.Gold)
+	text = str(PlayerStats.Orbs)
 	# just keeps track of the gold count. Nothing too fancy.
