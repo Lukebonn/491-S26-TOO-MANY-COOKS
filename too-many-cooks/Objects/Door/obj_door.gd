e793bc68db51b0a4e96582e66fc655b74e98f481
Luke Bonniwell <113559516+Lukebonn@users.noreply.github.com>
2026-04-12 20:36:52 -0400

    level and door mechanics
    updated door and room mechanics + minor level upgrades for lukelevel1.tscn (changes still in progress)

--- too-many-cooks/Objects/Door/obj_door.gd
+++ too-many-cooks/Objects/Door/obj_door.gd
@@ -2,25 +2,64 @@ extends Node2D
 
 var is_open = false
 #Whether or not this door is open, and thus enterable.
+var is_locked = false
+#Whether this door has been locked (usually changed by an outside scene)
 var animRef : AnimationPlayer
 #Ref to AnimationPlayer.
 var spriteRef : Sprite2D
 #Ref to Sprite2D.
+var camera
+#needs to get the camera for camera shake
 ##Whether or not this Door should start already open.
 @export var StartOpen = false
+
+@onready var door_closing: AudioStreamPlayer2D = $doorClosing
+@onready var door_opening: AudioStreamPlayer2D = $doorOpening
+@onready var door_unlocking: AudioStreamPlayer2D = $doorUnlocking
+
 
 func _ready() -> void:
 	animRef = $AnimationPlayer
 	spriteRef = $Sprite2D
 	is_open = StartOpen
+	camera = get_tree().get_first_node_in_group("camera")
 	if is_open:
 		spriteRef.frame = 3
 	else:
 		spriteRef.frame = 0
 
 func set_open(open: bool):
+	if is_locked:
+		return
+	if is_open == open:
+		return
 	is_open = open
 	if is_open:
 		animRef.play("open")
+		door_opening.play()
 	else:
 		animRef.play("close")
+		door_closing.play()
+
+func set_locked(locked: bool):
+	is_locked = locked
+	door_unlocking.play()
+	if camera:
+		camera.add_trauma(0.5)
+	
+	if is_locked:
+		$CollisionShape2D.set_deferred("disabled", false)
+		set_open(false)
+	else:
+		$CollisionShape2D.set_deferred("disabled", true)
+		set_open(true)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("door opened")
+	if body.name == "Player":
+		set_open(true)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("door closed")
+	if body.name == "Player":
+		set_open(false)
--- too-many-cooks/Objects/PlayerCamera/player_camera.gd
+++ too-many-cooks/Objects/PlayerCamera/player_camera.gd
@@ -1,11 +1,20 @@
 extends Camera2D
 @export var Player : CharacterBody2D
+#vars for camera shake
+@export var decay: float = 0.8
+@export var max_offset: Vector2 = Vector2(100, 75)
+@export var max_roll: float = 0.1
+
+var trauma: float = 0.0
+var trauma_power: int = 2
 
 #for if we want to move the camera away from the character sometimes 
 #like to introduce a boss or something
 var is_following_player = true
 var sensitivity = 60
 func _ready():
+	randomize()
+	add_to_group("camera")
 	#if there is no player set try to make it a sibling
 	if Player == null:
 		Player = $"../Player"
@@ -18,7 +27,23 @@ func _process(delta):
 	
 	if is_following_player:
 		position = Player.position + get_mouse_vector()/sensitivity
+	
+	#this is for the camera shake functionality and is constantly checking if the camera shake functionality has been called
+	if trauma: 
+		trauma = max(trauma - decay * delta, 0)
+		shake()
 
 func get_mouse_vector():
 	var value = Player.position + get_local_mouse_position()
 	return value
+
+#function to be called outside if want screen shake
+func add_trauma(amount: float) -> void:
+	#capping the screen shake at one pretty much, so even if "amount" is greater than one it will cap at one
+	trauma = min(trauma + amount, 1.0)
+
+func shake() -> void:
+	var amount = pow(trauma, trauma_power)
+	rotation = max_roll * amount * randf_range(-1,1)
+	offset.x = max_offset.x * amount * randf_range(-1,1)
+	offset.y = max_offset.y * amount * randf_range(-1,1)
