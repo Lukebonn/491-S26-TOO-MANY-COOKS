f89e2ccfffb1a3d1de4c25367fa8d928697de3c2
Owen Magee <owenmagee7@gmail.com>
2026-04-18 11:59:21 -0400

    Vortex spell now pulls enemies and does not deal damage

--- too-many-cooks/Characters/Player/Scripts/StateMachine/mage_class/Attacks/vortex.gd
+++ too-many-cooks/Characters/Player/Scripts/StateMachine/mage_class/Attacks/vortex.gd
@@ -1,10 +1,11 @@
 extends StaticBody2D
 
-var damage : int = int(PlayerStats.base_str * 1.1)
+#var damage : int = int(PlayerStats.base_str * 1.1)
 
 var enemies : Array
 
 ##sets the amount of damage the vortex should do per hit
+#unused as vortex has been reworked and no longer does damage to enemies
 func _ready() -> void:
 	#damage = int(get_parent().strength * 1.2)
 	pass
@@ -13,27 +14,18 @@ func _ready() -> void:
 ##applies velocity over time that drags enemies towards the center of the vortex
 func _process(delta: float) -> void:
 	for enemy in enemies:
-		if(enemy is Enemy):
-			enemy.velocity = (global_position - enemy.global_position).normalized() * 5000 * delta
+		if(enemy.global_position != global_position):
+			enemy.global_position += (global_position - enemy.global_position).normalized() * 55 * delta
 
 
 ##adds any enemy who enters the vortex to the list
-func _on_hitbox_area_entered(area: Area2D) -> void:
-	enemies.append(area.get_parent())
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	enemies.append(body)
 
 
 ##removes any enemy who leaves the vortex from the list
-func _on_hitbox_area_exited(area: Area2D) -> void:
-	enemies.erase(area.get_parent())
-
-
-##flicks the hitbox on and off every second so the enemies inside take repeated damage
-func _on_timer_timeout() -> void:
-	$Hitbox.monitorable = false
-	$Hitbox.monitoring = false
-	
-	$Hitbox.monitorable = true
-	$Hitbox.monitoring = true
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	enemies.erase(body)
 
 
 ##vortex deletes itself after 5 seconds
