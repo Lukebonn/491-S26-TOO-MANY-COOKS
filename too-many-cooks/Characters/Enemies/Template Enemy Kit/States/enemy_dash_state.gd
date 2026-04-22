fc00a711238186cd66e1a1c6ff9c15639f150d56
DBrownDHen <dbrownki@udel.edu>
2026-04-22 17:45:30 -0400

    Organizes Files & Polishes Dash State

--- too-many-cooks/Characters/Enemies/Template Enemy Kit/States/enemy_dash_state.gd
+++ too-many-cooks/Characters/Enemies/Template Enemy Kit/States/enemy_dash_state.gd
@@ -1,66 +1,57 @@
 extends EnemyState
 #unimplemented so far
 
-enum dashSteps {
-	align,
-	wait,
-	dash,
-	stop
-}
-
-var dashStep
 var xDifference
 var yDifference
+@export var speed: float
+# Works inversely compared to what you might be familiar with
+# Lower speed values increase the speed at which the enemy
+# dashes towards you
+
+var dashing = false
+signal dashComplete
 
 func enter_state(enemy_node):
 	super(enemy_node)
-	dashStep = dashSteps.align
 
 func process(delta: float) -> void:
-	if player_ref and enemy_ref:
-		match dashStep: #woawie a match statement
-			dashSteps.align:
-				yDifference = player_ref.position.y - enemy_ref.position.y
-				print(yDifference)
-				if abs(yDifference) < 0.5:
-					# if the difference in y-axis positions is very small,
-					# then the enemy and the player are nearly on the same
-					# y-level, so...
-					dashStep = dashSteps.wait # ...Move on to next step
-				elif (yDifference) < 0:
-					#print("Player is above me")
-					# if the difference is negative,
-					# then the player is above the enemy, so move up
-					enemy_ref.position += Vector2(0,-0.75)
-				elif (yDifference) > 0:
-					#print("Player is below me")
-					# if the difference is positive, 
-					# then the player is below the enemy, so move down
-					enemy_ref.position += Vector2(0,0.75)
-			dashSteps.wait:
-				await get_tree().create_timer(0.1).timeout
-				xDifference = player_ref.position.x - enemy_ref.position.x
-				dashStep = dashSteps.dash
-				# waits a moment, so the player has time to move,
-				# and then gets the distance the enemy is from the player
-				# when the dash should begin, and then move on to dashing
-			dashSteps.dash:
-				## THIS PART IS VERY ROUGH AROUND THE EDGES!
-				## I intend on polishing this up much more later.
-				var xDistance = player_ref.position.x - enemy_ref.position.x
-				#print(str(xDistance) + " " + str(xDifference))
-				if xDifference < 0: # negative -> player to left of enemy
-					enemy_ref.position += Vector2(-1, 0)
-					if xDistance >= 30:
-						dashStep = dashSteps.stop
-				elif xDifference > 0: # positive -> player to right of enemy
-					enemy_ref.position += Vector2(1, 0)
-					if xDistance <= -30:
-						dashStep = dashSteps.stop
-			dashSteps.stop:
-				# THIS STUFF TO BE REPLACED PROBABLY.
-				await get_tree().create_timer(0.5).timeout
-				dashStep = dashSteps.align
+	if player_ref and enemy_ref and not dashing:
+		yDifference = player_ref.position.y - enemy_ref.position.y
+		#print(yDifference)
+		if abs(yDifference) < 0.75:
+			# if the difference in y-axis positions is very small,
+			# then the enemy and the player are nearly on the same
+			# y-level, so...
+			await get_tree().create_timer(0.1).timeout
+			dashing = true # ...Move on to next step
+			xDifference = player_ref.position.x - enemy_ref.position.x
+			if xDifference < 0: # negative -> player to left of enemy
+				dash("Right")
+				await dashComplete
+				dashing = false
+			elif xDifference > 0: # positive -> player to right of enemy
+				#enemy_ref.position += Vector2(1, 0)
+				dash("Left")
+				await dashComplete
+				dashing = false
+		elif (yDifference) < 0:
+			#print("Player is above me")
+			# if the difference is negative,
+			# then the player is above the enemy, so move up
+			if yDifference > -1:
+				enemy_ref.position += Vector2(0, -0.3)
+			else:
+				enemy_ref.position += Vector2(0, -2)
+			#enemy_ref.position += Vector2(0, -2)
+		elif (yDifference) > 0:
+			#print("Player is below me")
+			# if the difference is positive, 
+			# then the player is below the enemy, so move down
+			if yDifference < 1:
+				enemy_ref.position += Vector2(0, 0.3)
+			else:
+				enemy_ref.position += Vector2(0, 2)
+			#enemy_ref.position += Vector2(0, 2)
 
 		
 	# The Plan:
@@ -75,10 +66,23 @@ func process(delta: float) -> void:
 	# chooses to either continue with a chase or a dash.
 
 func hit_response(source):
-	pass 
-	## Dawson changed this to pass so the whole dash procedure
-	## is not interrupted when the player hits the enemy during it.
-	#enemy_ref.change_state("HitState")
-	# because of this, hitting the enemy while they're dashing will
-	# break their dash, and hitting them will probably be very easy,
-	# so im not sure if this should be a feature.
+	pass #enemy_ref.change_state("HitState")
+
+func dash(direction: String):
+	var tween = self.create_tween()
+	xDifference = player_ref.position.x - enemy_ref.position.x
+	match direction:
+		"Right":
+			tween.tween_property(
+				enemy_ref, 
+				"position", 
+				Vector2(enemy_ref.position.x - abs(xDifference) - 35, enemy_ref.position.y), 
+				speed).set_trans(Tween.TRANS_EXPO)
+		"Left":
+			tween.tween_property(
+				enemy_ref, 
+				"position", 
+				Vector2(enemy_ref.position.x + abs(xDifference) + 35, enemy_ref.position.y), 
+				speed).set_trans(Tween.TRANS_EXPO)
+	await get_tree().create_timer(1.0).timeout
+	dashComplete.emit()
