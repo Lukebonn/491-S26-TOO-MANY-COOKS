64c92b65957ede18079d0d6af7a216e582b6325a
DBrownDHen <dbrownki@udel.edu>
2026-04-29 22:34:12 -0400

    Works on some Rat King Functions

--- too-many-cooks/Characters/Enemies/Rat King/damage_area.gd
+++ too-many-cooks/Characters/Enemies/Rat King/damage_area.gd
@@ -4,10 +4,3 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-func _on_create_damage_area(playerX: float, playerY: float) -> void:
-	await get_tree().create_timer(1.0).timeout
-	var copy = duplicate(DUPLICATE_USE_INSTANTIATION)
-	copy.get_child(0).position = Vector2(playerX, playerY)
-	position.x = playerX
-	position.y = playerY
--- too-many-cooks/Characters/Enemies/Rat King/multi_projectile_state.gd
+++ too-many-cooks/Characters/Enemies/Rat King/multi_projectile_state.gd
@@ -1,10 +1,40 @@
 extends EnemyState
 
+var targeting = false
+var projectile = preload("res://Characters/Enemies/Rat King/RKBasicProjectile.tscn")
 
 func enter_state(enemy_node):
 	super(enemy_node)
-	#await get_tree().create_timer(0.3).timeout
+	$"../RegularProjectileLines".hide()
+	$"../EnragedProjectileLines".hide()
+	if not enemy_ref.enraged: $"../RegularProjectileLines".show()
+	else: $"../EnragedProjectileLines".show()
+	targeting = true
+	await get_tree().create_timer(1.5).timeout
+	targeting = false
+	$"../RegularProjectileLines/Projectile Line1".target_position = Vector2(player_ref.position.x, player_ref.position.y - 56)
+	$"../RegularProjectileLines/Projectile Line2".target_position = player_ref.position
+	$"../RegularProjectileLines/Projectile Line3".target_position = Vector2(player_ref.position.x, player_ref.position.y + 56)
+	var projectile1 = projectile.instantiate()
+	projectile1.name = "1"
+	add_child(projectile1)
+	var projectile2 = projectile.instantiate()
+	projectile2.name = "2"
+	add_child(projectile2)
+	var projectile3 = projectile.instantiate()
+	projectile3.name = "3"
+	add_child(projectile3)
+	if enemy_ref.enraged:
+		var projectile4 = projectile.instantiate()
+		projectile4.name = "4"
+		add_child(projectile4)
+	projectile1.look_at(Vector2(player_ref.position.x, player_ref.position.y))
+	#projectile1.position = lerp_move_to_target(player_ref.position)
 
 func process(_delta):
-	$"../RegularProjectileLines".look_at(Vector2(player_ref.position.x, player_ref.position.y))
-	
+	pass
+	#if targeting:
+		#if not enemy_ref.enraged:
+			#$"../RegularProjectileLines".look_at(Vector2(player_ref.position.x, player_ref.position.y))
+		#else:
+			#$"../EnragedProjectileLines".look_at(Vector2(player_ref.position.x, player_ref.position.y))
