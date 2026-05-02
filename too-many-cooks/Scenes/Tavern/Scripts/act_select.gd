613fa4a8bc474eac1c20f3779641c151ed661cbc
YakuzaBalooza <jkrow124@gmail.com>
2026-05-01 22:34:01 -0400

    Act Select bug fix, More Credits
    - Fixed a fatal crash that would occur if the player clicked an Act Select button several times in succession.
    - Added more people to the Credits UI, still heavy WIP.

--- too-many-cooks/Scenes/Tavern/Scripts/act_select.gd
+++ too-many-cooks/Scenes/Tavern/Scripts/act_select.gd
@@ -1,5 +1,6 @@
 extends Control
 var is_showing = false
+var allow_input = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,12 +8,6 @@ func _ready():
 		$Panel/VBoxContainer/MarginContainer2/Levels/Act2.disabled = false
 	if Global.Act_3_Unlocked:
 		$Panel/VBoxContainer/MarginContainer2/Levels/Act3.disabled = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_close_pressed():
 	hide_menu()
@@ -34,51 +29,45 @@ func hide_menu_top():
 	tween.tween_property(self,"position",Vector2(self.position.x,-1000),.5).set_trans(Tween.TRANS_CUBIC)
 	is_showing = false
 
+#Act buttons clicked; make respective level queues.
 func _on_act_1_pressed():
-	#later hook this up to a level queue that picks N unique levels out of the scenes in a given folder
 	make_level_queue("res://Scenes/Level Design/Playtest3LevelPack/Act 1",3, 1)
-	pass # Replace with function body.
-
-
 func _on_act_2_pressed():
 	make_level_queue("res://Scenes/Level Design/Playtest3LevelPack/Act 2",4, 2)
-
-
 func _on_act_3_pressed():
 	make_level_queue("res://Scenes/Level Design/Playtest3LevelPack/Act 1",5, 3)
-	
-func make_level_queue(dir: String, amount : int, act: int):
-	var files = DirAccess.get_files_at(dir)
-	var levels: Array = []
-	for file in files:
-		file = dir + "/" + file
-		if file.right(5) == ".tscn" or "remap":
-			levels.append(file)
-	var indicies_queued: Array = []
-	print(files)
-	while LevelQueue.Queue.size() < amount-1:
-		var rng = randi_range(0,(levels.size()-1))
-		if rng not in indicies_queued:
-			var new_level = levels.get(rng)
-			LevelQueue.Queue.append(new_level)
-			print(new_level + " was not in level queue, adding it")
-			indicies_queued.append(rng) 
-		else:
-			print("attempted level was in the level queue already")
-			print(LevelQueue.Queue.size())
-			await get_tree().process_frame
-	match act:
-		1:
-			LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Bosses/Level1BossRoom.tscn")
-		2:
-			LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Bosses/Level2BossRoom.tscn")
-		3:
-			LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Bosses/Level3BossRoom.tscn")
-	print("Level queue is... " + str(LevelQueue.Queue))
-	LevelQueue.load_level()
-		
-		
 
+func make_level_queue(dir: String, amount : int, act: int):
+	if allow_input:
+		allow_input = false
+		var files = DirAccess.get_files_at(dir)
+		var levels: Array = []
+		for file in files:
+			file = dir + "/" + file
+			if file.right(5) == ".tscn" or "remap":
+				levels.append(file)
+		var indicies_queued: Array = []
+		print(files)
+		while LevelQueue.Queue.size() < amount-1:
+			var rng = randi_range(0,(levels.size()-1))
+			if rng not in indicies_queued:
+				var new_level = levels.get(rng)
+				LevelQueue.Queue.append(new_level)
+				print(new_level + " was not in level queue, adding it")
+				indicies_queued.append(rng) 
+			else:
+				print("attempted level was in the level queue already")
+				print(LevelQueue.Queue.size())
+				await get_tree().process_frame
+		match act:
+			1:
+				LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Bosses/Level1BossRoom.tscn")
+			2:
+				LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Bosses/Level2BossRoom.tscn")
+			3:
+				LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Bosses/Level3BossRoom.tscn")
+		print("Level queue is... " + str(LevelQueue.Queue))
+		LevelQueue.load_level()
 
 func _on_practice_pressed():
 	LevelQueue.Queue.append("res://Scenes/Level Design/Playtest3LevelPack/Practice.tscn")
