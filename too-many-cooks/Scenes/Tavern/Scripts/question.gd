aa14d10f96e899f0d94207fcfc3cd759a2360065
CSD-business <143720980+CSD-business@users.noreply.github.com>
2026-05-03 19:32:30 -0400

    Removed fancy apostrophes and elipses in the txt files and replaced them with normal ones(they would not render on web)
    I love google docs

--- too-many-cooks/Scenes/Tavern/Scripts/question.gd
+++ too-many-cooks/Scenes/Tavern/Scripts/question.gd
@@ -12,24 +12,24 @@ func _on_pressed():
 			dialogue_ref.show_dialogue("Warrior","7,8,16,17,18,19,20,21,22,23","0,0")
 		"\"I almost had it.\"":
 			dialogue_ref.show_dialogue("Warrior","10,11,16,17,18,19,20,21,22,23","0,0")
-		"\"I don’t want to go back.\"":
+		"\"I don't want to go back.\"":
 			dialogue_ref.show_dialogue("Warrior","13,14,16,17,18,19,20,21,22,23","0,0")
 	
-		"\"I’ll help the tavern.\"":
+		"\"I'll help the tavern.\"":
 			dialogue_ref.show_dialogue("Warrior","25,26,34,35","0,0")
-		"\"What’s happening at the tavern?\"":
+		"\"What's happening at the tavern?\"":
 			dialogue_ref.show_dialogue("Warrior","28,29,34,35","0,0")
 		"\"Does it really matter?\"":
 			dialogue_ref.show_dialogue("Warrior","31,32,34,35","0,0")
 	
-		"\"I don’t feel like a warrior.\"":
+		"\"I don't feel like a warrior.\"":
 			dialogue_ref.show_dialogue("Warrior","37,38,46,47,48,49,50","0,0")
 		"\"I want to get stronger.\"":
 			dialogue_ref.show_dialogue("Warrior","40,41,46,47,48,49,50","0,0")
-		"\"I’m not sure I can do this again.\"":
+		"\"I'm not sure I can do this again.\"":
 			dialogue_ref.show_dialogue("Warrior","43,44,46,47,48,49,50","0,0")
 		
-		"\"I’ll help fix this place.\"":
+		"\"I'll help fix this place.\"":
 			dialogue_ref.show_dialogue("Warrior","52,58,59,60,61,62,63,64,65","0,0")
 		"\"This place suits you.\"":
 			dialogue_ref.show_dialogue("Warrior","54,58,59,60,61,62,63,64,65","0,0")
