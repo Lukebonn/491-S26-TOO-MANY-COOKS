ef1b802008867e5d3a320ac970aac0a85a14875e
YakuzaBalooza <jkrow124@gmail.com>
2026-05-02 20:27:11 -0400

    Credits Progress
    - Added everyone to the Credits UI, added Ata's portrait.

--- too-many-cooks/UI/Credits/Scripts/credits.gd
+++ too-many-cooks/UI/Credits/Scripts/credits.gd
@@ -9,3 +9,14 @@ func _ready() -> void:
 	FadeInFadeOut.fade_in()
 	var tween = get_tree().create_tween()
 	tween.tween_property(self,"position",Vector2(0,TweenTarget),TweenTime).set_trans(Tween.TRANS_LINEAR)
+	#await tween.finished
+	#wait_and_fade()
+#
+#func wait_and_fade() -> void:
+	#await get_tree().create_timer(3.0).timeout
+	#FadeInFadeOut.fade_out()
+	#await get_tree().create_timer(1.2).timeout
+	#get_tree().change_scene_to_file("res://Scenes/Title/title_screen.tscn")
+
+# Above code waits then fades back to title.
+# Commented out in case we want the Credits UI to just show in title on top of background.
