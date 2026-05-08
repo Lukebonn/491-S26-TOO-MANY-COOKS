cc9a063d17b5f8b7b1a065a54fe93a1068f508b1
CSD-business <143720980+CSD-business@users.noreply.github.com>
2026-05-08 11:56:32 -0400

    Adds particles and makes enemies aggro on to you when you hit them

--- too-many-cooks/Characters/Enemies/Particles/DustParticles.gd
+++ too-many-cooks/Characters/Enemies/Particles/DustParticles.gd
@@ -1,13 +1,4 @@
 extends Node2D
 
-@onready var particles = $GPUParticles2D
-
-func _ready():
-	print("DUST READY, particles node: ", particles)
-	if particles:
-		particles.emitting = true
-		print("EMITTING: ", particles.emitting)
-		print("AMOUNT: ", particles.amount)
-		print("LIFETIME: ", particles.lifetime)
-	await get_tree().create_timer(1.0).timeout
+func _on_animated_sprite_2d_animation_finished():
 	queue_free()
